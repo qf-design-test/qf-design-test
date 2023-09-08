e87879746f7b852b838ed576b3340e9f1cfc1f30
wangkai <1205669217@qq.com>
2023-09-08 11:39:11 +0800

    feat: update commitlint rules

--- commitlint.config.js
+++ commitlint.config.js
@@ -1,4 +1,7 @@
 // eslint-disable-next-line no-undef
 module.exports = {
-  extends: ['@commitlint/config-conventional']
+  extends: ['@commitlint/config-conventional'],
+  rules: {
+    'subject-case': [2, 'never', ['upper-case', 'pascal-case', 'start-case']]
+  }
 };
